a5ea1e3636107fc35dab27c7759f8ee522aa1796
Evandro Lutz <evandrolutz@outlook.com>
2022-01-03 15:54:28 -0300

    validacao de usuario na home

--- app/src/main/java/com/example/gasapp/ui/home/HomeViewModel.java
+++ app/src/main/java/com/example/gasapp/ui/home/HomeViewModel.java
@@ -4,13 +4,43 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.gasapp.model.PedidoTotal;
+import com.example.gasapp.model.Pessoa;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 public class HomeViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
     public HomeViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is home fragment");
+        DatabaseReference rootRef = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference userNameRef = rootRef.child("pessoa");
+        ValueEventListener eventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.exists()) {
+                    mText.setValue("Cadastre seu endereço para efetuar um pedido");
+
+                } else {
+
+                    Pessoa pessoa = dataSnapshot.getValue(Pessoa.class);
+                    mText.setValue("Olá "+ pessoa.getNome() + ", faça seu pedido para ser entregue em "+ pessoa.getEndereco());
+
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {}
+        };
+        userNameRef.addListenerForSingleValueEvent(eventListener);
+
     }
 
     public LiveData<String> getText() {
